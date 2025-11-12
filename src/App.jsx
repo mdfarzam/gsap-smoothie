690e147a17e312b37d82532107076c4e65bf3167
mdfarzam <mohammedfarzam02@gmail.com>
2025-11-13 00:56:50 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
 
 const App = () => {
   return (
-    <div>
-       
-    </div>
+    <main>
+      <NavBar/>
+      <HeroSection/>
+    </main>
   )
 }
 
